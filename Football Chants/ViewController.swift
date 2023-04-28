48c0e43969e4aae92edb01db6539df5a9d7cc00d
SevarJafarli <sevar.jafarli.std@bhos.edu.az>
2023-04-28 19:41:44 +0400

    Initial Commit

---
+++ Football Chants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football Chants
+//
+//  Created by Sevar Jafarli on 28.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
